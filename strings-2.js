6992580077f6171ffab097c6762856ddd2f6d154
Michael Seth <michael.a.seth175@gmail.com>
2021-02-02 16:08:21 -0500

    added replaceAll

--- strings-2.js
+++ strings-2.js
@@ -2,3 +2,7 @@ console.log ("hello")
 const userName = prompt("Enter your first name...")
 const greeting =`How are you doing today, `
 document.getElementById('output').textContent = greeting + userName + " ?"
+
+userText = prompt("Enter some text here:");
+userText = userText.replaceAll( "a", "@");
+document.getElementById('output').textContent = userText;
