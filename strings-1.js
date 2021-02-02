4b04767fd55347b35b3d580eb174d7d8df3a2554
Michael Seth <michael.a.seth175@gmail.com>
2021-02-02 15:26:42 -0500

    added charAt

--- strings-1.js
+++ strings-1.js
@@ -1,5 +1,6 @@
 console.log("Let's do this!");
-let userText = prompt("Please add text add:");
+let userText = prompt("Please add text here:");
 alert(`The number of characters from my text is: ${userText.length}`);
 console.log(userText.indexOf("l"))
 console.log(userText.slice(3))
+console.log(userText.charAt(userText.length -1));
