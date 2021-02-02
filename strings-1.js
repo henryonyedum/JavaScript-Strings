e9ef7bafce902f6a1135525bcb7eb9576772b44e
bowenstone <bowenstone@comcast.net>
2021-02-02 15:20:18 -0500

    add slice to js code

--- strings-1.js
+++ strings-1.js
@@ -1,4 +1,5 @@
 console.log("Let's do this!");
 let userText = prompt("Please add text add:");
 alert(`The number of characters from my text is: ${userText.length}`);
-console.log(userText.indexOf("L"))
+console.log(userText.indexOf("l"))
+console.log(userText.slice(3))
