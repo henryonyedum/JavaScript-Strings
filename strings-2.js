df72fc9cad1c614e4a64898756148740f3ccbc63
bowenstone <bowenstone@comcast.net>
2021-02-02 15:54:01 -0500

    add DOM

--- strings-2.js
+++ strings-2.js
@@ -1,1 +1,4 @@
 console.log ("hello")
+const userName = prompt("Enter your first name...")
+const greeting =`How are you doing today, `
+document.getElementById('output').textContent = greeting + userName + " ?"
